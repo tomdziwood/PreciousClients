e8251f80d5f2ef2704079147752335879e1b24a9
tomdziwood <tomdziwood@gmail.com>
2021-05-24 21:09:12 +0200

    Parametryzowany skrypt produkuje końcowy plik wyjściowy output.dat

--- present_precious_clients.py
+++ present_precious_clients.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 
 from database import create_connection
 
@@ -38,14 +39,23 @@ def main():
     accepted_counter = 0
     denied_counter = 0
     denied_limit = 200
-    for row in result:
-        if (row[9] is not None) and (row[9] > args.vip_income) and (denied_counter < denied_limit):
-            denied_counter += 1
-            # print("VIP_INCOME threshold is blocking")
-            continue
 
-        accepted_counter += 1
-        print(row)
+    with open("output.dat", "w", newline="", encoding='utf-8') as f:  # On Python 3.x use "w" mode and newline=""
+        writer = csv.writer(f, delimiter="|")
+        for row in result:
+            if (row[9] is not None) and (row[9] > args.vip_income) and (denied_counter < denied_limit):
+                denied_counter += 1
+                # print("VIP_INCOME threshold is blocking")
+                continue
+
+            accepted_counter += 1
+            # print(row)
+            row_formatted = list(row)
+            row_formatted[4] = row_formatted[4].ljust(110)
+            row_formatted[5] = row_formatted[5].ljust(40)
+            row_formatted[6] = row_formatted[6].ljust(50)
+            row_formatted[7] = row_formatted[7].rjust(5, '0')
+            writer.writerow(row_formatted)
 
     print("Number of all records:\t\t" + str(all_counter))
     print("Number of denied by VIP_INCOME:\t" + str(denied_counter))
